afee70a6588d5e60322201355656059b6c1b9f3e
Aniket Patidar <aniketpatidar01@gmail.com>
2026-08-27 17:57:35 +0530

    [main] Fix Next.js config and TS errors for Cloudflare build

--- apps/web-cms/next.config.ts
+++ apps/web-cms/next.config.ts
@@ -59,4 +59,6 @@ const nextConfig: NextConfig = {
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
 
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+if (process.env.NODE_ENV === 'development') {
+  import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+}
